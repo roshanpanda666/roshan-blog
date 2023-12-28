528ca0fe120385357ea9da7022a39d8212edfb97
Roshan panda <roshanpanda666@gmail.com>
2023-12-28 17:06:35 +0530

    commitinggggg

--- pages/components/nav.js
+++ pages/components/nav.js
@@ -5,7 +5,7 @@ const Nav = () => {
     <div>
       <div className="flex flex-row lg:gap-[60vw] gap-16 justify-center item-center mt-6">
         <Link href='/'>
-        <div className="lg:text-[2rem] text-1xl bg-gradient-to-r from-cyan-400 via-violet-600 to-violet-700 bg-clip-text text-transparent filter drop-shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] font-mono ">Sabyasachi's Blog</div>
+        <div className="lg:text-[2rem] text-[0.8rem] bg-gradient-to-r from-cyan-400 via-violet-600 to-violet-700 bg-clip-text text-transparent filter drop-shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] font-mono ">Sabyasachi's Blog</div>
         </Link>
 
         <div className="flex justify-center items-center gap-11">
