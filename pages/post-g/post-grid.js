8a03594a09448d001ca5899abb871a8c6a987f65
Roshan panda <roshanpanda666@gmail.com>
2023-12-29 17:32:21 +0530

    commitingggggggggggggg

--- pages/post-g/post-grid.js
+++ pages/post-g/post-grid.js
@@ -6,7 +6,7 @@ const PostGrid = (props) => {
   return (
     <div>
         <div className=' mt-14 flex flex-wrap gap-10 justify-center items-center text-center'>
-            {posts.map((post)=>(<PostItem key={post.slug} post={post}></PostItem>))}
+        {posts && posts.map((post) => (<PostItem key={post.slug} post={post} />))}
 
         </div>
     </div>
